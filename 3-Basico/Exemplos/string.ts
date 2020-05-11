724779c1a7e0c71c7ed1b82636972f2e515d4b4f
Thiago <thiago.lopes.dev@gmail.com>
2020-05-11 14:45:54 -0300

    add string exercice

---
+++ 3-Basico/Exemplos/string.ts
@@ -0,0 +1,16 @@
+// Strings
+
+let cor: string = "azul"
+cor = "vermelho"
+
+console.log(cor) // saída: vermelho
+
+let nomeCompleto: string = `Thiago Lopes`
+let idade: number = 22
+let frase: string = `Oi, meu nome é 
+    ${nomeCompleto} e vou completar 
+    ${idade + 1} anos esse ano!`
+
+console.log(frase) // saída: Oi, meu nome é Thiago Lopes e vou completar 23 anos esse ano!
+
+
