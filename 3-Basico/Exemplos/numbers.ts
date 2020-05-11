c4ae92609251e965f781821378c4aec80e1b9a00
Thiago <thiago.lopes.dev@gmail.com>
2020-05-11 14:45:31 -0300

    add numbers exercice

---
+++ 3-Basico/Exemplos/numbers.ts
@@ -0,0 +1,8 @@
+let numero: number = 123;
+
+console.log(numero) // saída: 123
+
+/* diferente do Javascript você não pode associar strings a váriaveis que tenham
+number em sua declaração. */
+
+numero = '123' // Erro!
