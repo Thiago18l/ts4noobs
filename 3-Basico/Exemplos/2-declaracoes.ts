749fabac35080071b5dbdcebdc1cb8cb062fadf3
Thiago <thiago.lopes.dev@gmail.com>
2020-05-13 16:05:45 -0300

    add declaraçoes e inferencia de tipo no ts

---
+++ 3-Basico/Exemplos/2-declaracoes.ts
@@ -0,0 +1,9 @@
+let variavel: number = 123
+//variavel = '123' // ERRO!
+// Tipo '"123"' não é atribuível  para o tipo 'number'.
+
+let meuNumero: number = 10
+console.log(meuNumero)
+
+let DeixaEuDescobrir = "123"
+console.log(typeof(DeixaEuDescobrir))
