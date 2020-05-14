d029c695126e36bebda9353e3551af3016afdc7d
Thiago <thiago.lopes.dev@gmail.com>
2020-05-14 14:53:34 -0300

    escopo de variaveis e seu uso

---
+++ 3-Basico/Exemplos/3-escopo.ts
@@ -0,0 +1,20 @@
+/**
+ * OBS: Os console.log() estão 
+ * comentados para não dar 
+ * erro de compilação.
+ */
+
+if (true) {
+    var x: number = 5
+}
+// console.log(x) // saída: 5
+
+if (true) {
+    let y: number = 5
+}
+// console.log(y) // erro, y não está definido
+
+if (true) {
+    const PI: number = 3.14
+}
+// console.log(PI) // Erro, PI não está definido.
