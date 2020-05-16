e250961c06308a00947b2667984be382ad5111bf
Thiago <thiago.lopes.dev@gmail.com>
2020-05-16 03:59:11 -0300

    add exemplos para operadores relacionais

---
+++ 3-Basico/Exemplos/6.4-operadores.ts
@@ -0,0 +1,15 @@
+// Maior que
+let relacionais = (2 > 4) // false
+// Menor que
+relacionais = (2 < 4) // true
+// Maior ou igual
+relacionais = (2 >= 4) // false
+// Menor ou igual
+relacionais = (2 <= 4) // true
+// Igualdade
+let value1 = 2
+let value2 = 4
+relacionais = (value1 == value2) // false
+// Diferente de
+relacionais = (value1 != value2) // true
+
