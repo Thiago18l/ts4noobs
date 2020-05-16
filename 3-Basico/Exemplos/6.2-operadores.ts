f11af4c431e0ccb6a1b0619f3fa74034bc961614
Thiago <thiago.lopes.dev@gmail.com>
2020-05-16 03:58:39 -0300

    add exemplos para operadores aritmeticos

---
+++ 3-Basico/Exemplos/6.2-operadores.ts
@@ -0,0 +1,53 @@
+// Adição:
+
+let numero1 = 2
+
+numero1 += numero1
+
+console.log(numero1) // saída: 4
+
+// Subtração:
+
+let subtracao = 3
+
+subtracao -= 2
+
+console.log(subtracao)
+
+// Multiplicação
+
+let multiplicacao = 3
+
+multiplicacao *= multiplicacao // estou colocando ele mesmo no exemplo mas poderia ser qualquer outro número
+
+console.log(multiplicacao) // saída: 9
+
+// Divisão
+
+let divisao = 4
+
+divisao /= 2
+
+console.log(divisao) // saída: 2
+
+// Modulo ( resto da divisão )
+
+let resto = 10
+
+resto %= 2
+
+console.log(resto) // saída: 0
+
+// Incremento
+
+let a = 10
+a++
+
+console.log(a) // saída: 11
+
+// Decremento 
+let b = 10
+b-- 
+
+console.log(b) // saída: 9
+
