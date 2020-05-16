fb0962f236ebc362742b1ccc062a671e653f3eaf
Thiago <thiago.lopes.dev@gmail.com>
2020-05-16 03:59:26 -0300

    add exemplos para operadores bitwise

---
+++ 3-Basico/Exemplos/6.5-operadores.ts
@@ -0,0 +1,27 @@
+// OPERADORES BITWISE
+
+let result;
+
+// Operador AND
+result = (2 & 3) // 
+console.log(result) // 2
+
+//Operador OR
+result = ( 2 | 3 )
+console.log(result) // 3
+
+//Operador XOR
+result = (2 ^ 3)
+console.log(result) // 1
+
+//Operador NOT
+result = (~3)
+console.log(result) // -4
+
+// Operador left shift
+result = (2 << 1)
+console.log(result) // 4 
+
+// Operador right shift
+result = (2 >> 1)
+console.log(result) // 1
