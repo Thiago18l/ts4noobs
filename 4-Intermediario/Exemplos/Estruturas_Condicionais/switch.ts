a036f5fc614acfccce560d467139d92493260e88
Thiago <thiago.lopes.dev@gmail.com>
2020-06-03 01:14:55 -0300

    chore: add switch  example

---
+++ 4-Intermediario/Exemplos/Estruturas_Condicionais/switch.ts
@@ -0,0 +1,18 @@
+let grau: string = "A";
+
+switch (grau) {
+    case "A": {
+        console.log("Excelente!")
+        break;
+    }
+    case "B": {
+        console.log("Muito bom!")
+        break;
+    }
+    case "C": {
+        console.log("Justo!")
+    }
+    default: {
+        console.log("Escolha inválida!")
+    }
+}
