8de58b5fceaf60847689a33f01b5b7020034999a
Thiago <thiago.lopes.dev@gmail.com>
2020-06-03 01:14:39 -0300

    chore: add if-else example

---
+++ 4-Intermediario/Exemplos/Estruturas_Condicionais/if_else.ts
@@ -0,0 +1,28 @@
+// Exemplo 1
+
+let idade: number = 15; // Você pode alterar o número se desejar...
+
+if (idade >= 18) {
+    console.log(`Parabéns você já tem idade de tirar sua carteira!`)
+}
+
+console.log(`Fim!`)
+
+// Exemplo 2
+
+let numero: number = 10;
+
+if (numero % 2 == 0) {
+    console.log(`O numero ${numero} é par!`);
+} else {
+    console.log(`O numero ${numero} é ímpar!`)
+}
+
+// Exemplo 3
+
+numero = 100;
+if (numero <= 100) {
+    console.log("Menor ou igual a 100")
+} else if (numero < 1000) {
+    console.log("Menor que 1000")
+}
